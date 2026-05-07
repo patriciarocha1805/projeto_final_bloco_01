c2d44a540446e039f15b3ab726a9eb0e686885c2
Patrícia Rocha <ptc.patirocha@gmail.com>
2026-05-07 15:57:13 -0300

    Ajuste Terceira Etapa

--- src/projeto_final_bloco_01/controller/ProdutoController.java
+++ src/projeto_final_bloco_01/controller/ProdutoController.java
@@ -79,4 +79,6 @@ public class ProdutoController implements Repository {
 
 	}
 
+	
+
 }
--- src/projeto_final_bloco_01/menu/Menu.java
+++ src/projeto_final_bloco_01/menu/Menu.java
@@ -1,26 +1,22 @@
 package projeto_final_bloco_01.menu;
 
 import java.util.Scanner;
-
 import projeto_final_bloco_01.controller.ProdutoController;
 import projeto_final_bloco_01.model.ProdutoEscolar;
 
 public class Menu {
+	
+	private static final Scanner leia = new Scanner(System.in);
+
+	private static ProdutoController produtoController = new ProdutoController();
 
 	public static void main(String[] args) {
 		
-		Scanner leia = new Scanner(System.in);
+		int opcao;
 		
-		int opcao, quantidade, tipoPagamento;
-		float valor, valorTotal;
-		boolean continuar;
-		String nome;
-		
-		// Teste Controller
-		ProdutoController produtos = new ProdutoController();
+		// Teste Controller 
 		ProdutoEscolar produto1 = new ProdutoEscolar(1, "Caderno de Desenho", 30.00f, 2, "Artes");
-		produtos.cadastrar(produto1);
-		produtos.listarTodos();
+		produtoController.cadastrar(produto1);
 
 		while (true) {
 
@@ -30,9 +26,9 @@ public class Menu {
 			System.out.println("                                                     ");
 			System.out.println("*****************************************************");
 			System.out.println("                                                     ");
-			System.out.println("            1 - Cadastrar Produto                       ");
+			System.out.println("            1 - Cadastrar Produto                    ");
 			System.out.println("            2 - Pesquisar produto                    ");
-			System.out.println("            3 - Listar Produtos                             ");
+			System.out.println("            3 - Listar Produtos                      ");
 			System.out.println("            4 - Atualizar Cadastro                   ");
 			System.out.println("            5 - Apagar Conta                         ");
 			System.out.println("            0 - Sair                                 ");
@@ -51,31 +47,88 @@ public class Menu {
 			switch (opcao) {
 			case 1:
 				System.out.println("Criar Cadastro\n\n");
-					produto1.visualizar();
 				
-
+				produto1.visualizar(); 
+			    produtoController.cadastrar(produto1);
+				keyPress();
 				break;
+				
 			case 2:
 				System.out.println("Pesquisar Produto\n\n");
-
+				procurarProdutoPorCodigo();
+				keyPress();
 				break;
+				
 			case 3:
-				System.out.println("Carrinho de Compras\n\n");
-
+				System.out.println("Listar Produtos\n\n");
+				produtoController.listarTodos();
+				keyPress();
 				break;
+				
 			case 4:
 				System.out.println("Atualizar Cadastro\n\n");
-
+				atualizarProduto();
+				keyPress();
 				break;
+				
 			case 5:
 				System.out.println("Apagar o Cadastro\n\n");
-				
+				apagarProduto();
+				keyPress();
 				break;
-
+			default:
+				System.out.println("\nOpção Inválida!");
+				keyPress();
+				break;
+			}
+		}
 	}
+	
+	public static void keyPress() {
+		System.out.println("Pressione Enter para Continuar ...");
+		leia.nextLine();
+	}
+	
+	private static void procurarProdutoPorCodigo() {
+	    System.out.print("Digite o código do produto: ");
+	    int numero = leia.nextInt();
+	    
+	    produtoController.procurarPorCodigo(numero); 
+	}
+	
+	private static void atualizarProduto() {
+	    System.out.println("Digite o código do produto: ");
+	    int codigo = leia.nextInt();
+	    leia.nextLine();
+
+	    var buscaProduto = produtoController.buscarNaCollection(codigo);
+
+	    if (buscaProduto != null) {
+	        System.out.println("Digite o novo Nome: ");
+	        String nome = leia.nextLine();
+	        System.out.println("Digite o novo Preço: ");
+	        float preco = leia.nextFloat();
+	        System.out.println("Digite a nova Quantidade: ");
+	        int qtd = leia.nextInt();
+	        
+	        produtoController.atualizar(new ProdutoEscolar(codigo, nome, preco, qtd, "Artes"));
+	    } else {
+	        System.out.println("Produto não encontrado!");
+	    }
+	}
+	
+	private static void apagarProduto() {
+		System.out.println("Digite o código do produto: ");
+		int codigo = leia.nextInt();
+		leia.nextLine();
+		
+		System.out.println("\nTem certeza que deseja excluir? (S/N): ");
+		String confirmacao = leia.nextLine();
+		
+		if (confirmacao.equalsIgnoreCase("S")) {
+			produtoController.deletar(codigo);
+		} else {
+			System.out.println("\nOperação cancelada.");
 		}
 	}
 }
-
-
-
